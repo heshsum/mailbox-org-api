c27c2818eedcdf3afde7bf73545ca401dd299862
heshsum <mail@heshsum.de>
2024-11-02 08:43:23 +0100

    Added file structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mailbox-api-client',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A library to access the mailbox.org Business API',
+    author='Hendrik Schlange',
+    install_requires=['requests'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
