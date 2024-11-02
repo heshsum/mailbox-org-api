465f2865dbd8a2b4fc275080f04fa32f1b33c289
heshsum <mail@heshsum.de>
2024-11-02 17:40:02 +0100

    Renamed library

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mailbox-api-client',
+    name='mailboxOrgAPI',
     packages=find_packages(),
     version='0.1.0',
     description='A library to access the mailbox.org Business API',
