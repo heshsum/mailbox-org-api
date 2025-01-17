18656f258c5161d8855ca2b00166a85a439152a6
heshsum <mail@heshsum.de>
2025-01-17 16:03:29 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mailbox_org_api',
     packages=find_packages(),
-    version='0.9',
+    version='0.9.1',
     description='A library to access the mailbox.org Business API',
     author='Hendrik Schlange',
     install_requires=['requests'],
