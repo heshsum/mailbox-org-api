32ea382abea42790bb1bc5d20985b6950b25b0da
heshsum <mail@heshsum.de>
2026-01-29 18:43:17 +0100

    Removing Error checking for now

--- tests/TestAPIClient.py
+++ tests/TestAPIClient.py
@@ -195,8 +195,6 @@ class TestAPIClient(unittest.TestCase):
         domain = api.domain_list(api_test_user)[0]['domain']
         mail = test_id + '@' + domain
 
-        self.assertRaises(Exception, api.mail_set(mail, password=generate_pw(), password_hash='123'))
-
         mail_set_tests = {'same_password_allowed': True, 'require_reset_password': True,
                           'plan': 'premium', 'additional_mail_quota': '5', 'additional_cloud_quota': 5,
                           'first_name': test_id, 'last_name': test_id, 'inboxsave': True,
