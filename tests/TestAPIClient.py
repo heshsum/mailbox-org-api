9b73404c6fde7c1b758854dfca527a2fe647c7f7
heshsum <mail@heshsum.de>
2026-01-31 19:36:20 +0100

    Refactored TestAPIClient to use pytest

--- tests/TestAPIClient.py
+++ tests/TestAPIClient.py
@@ -1,6 +1,6 @@
 from mailbox_org_api import APIClient
 from mailbox_org_api.APIError import APIError
-import unittest
+import pytest
 import os
 import time
 import secrets
@@ -11,19 +11,22 @@ api_test_pass = os.environ['API_TEST_PASS']
 
 # Create a unique ID String by using the Unix time,
 # converted to int (to get rid of the decimal) and then to String
-test_id = str(int(time.time()))
+def generate_id():
+    return str(int(time.time()))
 
 def generate_pw():
     alphabet = string.ascii_letters + string.digits
     password = ''.join(secrets.choice(alphabet) for i in range(42))
     return password
 
-class TestAPIClient(unittest.TestCase):
+test_id = generate_id()
+
+class TestAPIClient:
     def test_headers(self):
         api = APIClient.APIClient()
-        self.assertEqual(api.auth_id, None)
-        self.assertEqual(api.level, None)
-        self.assertEqual(api.jsonrpc_id, 0)
+        assert api.auth_id is None
+        assert api.level is None
+        assert api.jsonrpc_id is 0
 
     #def test_hello_world(self):
     #    api = APIClient.APIClient()
@@ -33,53 +36,53 @@ class TestAPIClient(unittest.TestCase):
 
     def test_API_error(self):
         api = APIClient.APIClient()
-        with self.assertRaises(APIError):
+        with pytest.raises(APIError):
             api.auth('wröng_üser?', 'wröng_pässwörd!')
 
     def test_login(self):
         api = APIClient.APIClient()
-        self.assertEqual(api.jsonrpc_id, 0)
+        assert api.jsonrpc_id == 0
         api.auth(api_test_user, api_test_pass)
-        self.assertNotEqual(api.auth_id, None)
-        self.assertEqual(api.level, 'account')
-        self.assertEqual(api.jsonrpc_id, 1)
+        assert api.auth_id is not None
+        assert api.level == 'account'
+        assert api.jsonrpc_id == 1
         api.deauth()
 
     def test_account_get(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         account = api.account_get(api_test_user)
-        self.assertEqual('test_bmbo_api', account['account'])
-        self.assertEqual(account['type'], 'BMBO')
-        self.assertEqual(account['status'], 'aktiv')
-        self.assertEqual(account['language'], 'de_DE')
-        self.assertEqual(account['plan'], 'basic')
-        self.assertEqual(account['company'], 'test_bmbo_api')
-        self.assertEqual(account['ustid'], 'DE1234567')
+        assert account['account'] == 'test_bmbo_api'
+        assert account['type'] == 'BMBO'
+        assert account['status'] == 'aktiv'
+        assert account['language'] == 'de_DE'
+        assert account['plan'] == 'basic'
+        assert account['company'] == 'test_bmbo_api'
+        assert account['ustid'] == 'DE1234567'
         api.deauth()
 
     def test_account_get_object(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         account = api.account_get_object(api_test_user)
-        self.assertEqual(account.account, 'test_bmbo_api')
-        self.assertEqual(account.type, 'BMBO')
-        self.assertEqual(account.status, 'aktiv')
-        self.assertEqual(account.language, 'de_DE')
-        self.assertEqual(account.plan, 'basic')
-        self.assertEqual(account.company, 'test_bmbo_api')
-        self.assertEqual(account.ustid, 'DE1234567')
+        assert account.account == 'test_bmbo_api'
+        assert account.type == 'BMBO'
+        assert account.status == 'aktiv'
+        assert account.language == 'de_DE'
+        assert account.plan == 'basic'
+        assert account.company == 'test_bmbo_api'
+        assert account.ustid == 'DE1234567'
         api.deauth()
 
     def test_account_set(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         api.account_set(api_test_user, memo=test_id)
-        self.assertEqual(api.account_get(api_test_user)['memo'], test_id)
+        assert api.account_get(api_test_user)['memo'] == test_id
 
         test_id2 = str(int(time.time()))
         api.account_set(api_test_user, memo=test_id2)
-        self.assertEqual(api.account_get(api_test_user)['memo'], test_id2)
+        assert api.account_get(api_test_user)['memo'] == test_id2
         api.deauth()
 
     def test_account_invoice_list(self):
@@ -87,49 +90,50 @@ class TestAPIClient(unittest.TestCase):
         api.auth(api_test_user, api_test_pass)
         invoices = api.account_invoice_list(api_test_user)
         for invoice in invoices:
-            self.assertTrue(str.startswith(invoice['invoiceNumber'], 'BMBO-'))
-        self.assertEqual('2025-10-30', invoices[0]['date'])
-        self.assertEqual(0, invoices[0]['amount'])
-        self.assertEqual('invoice', invoices[0]['paymentType'])
-        self.assertEqual('storniert', invoices[0]['status'])
-        self.assertEqual(['csv', 'pdf', 'xml'], invoices[0]['availableDownloadFileTypes'])
-        self.assertEqual('BMBO-83002-25', invoices[0]['invoice_id'])
-        self.assertIsNotNone(invoices[0]['token'])
+            assert str.startswith(invoice['invoiceNumber'], 'BMBO-')
+        assert invoices[0]['date'] == '2025-10-30'
+        assert invoices[0]['amount'] == 0
+        assert invoices[0]['paymentType'] == 'invoice'
+        assert invoices[0]['status'] == 'storniert'
+        assert invoices[0]['availableDownloadFileTypes'] == ['csv', 'pdf', 'xml']
+        assert invoices[0]['invoice_id'] == 'BMBO-83002-25'
+        assert invoices[0]['token'] is not None
         api.deauth()
 
     def test_account_invoice_get_list(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         invoices = api.account_invoice_get_list(api_test_user)
-        self.assertIsNotNone(invoices)
+        assert invoices is not None
         for invoice in invoices:
-            self.assertTrue(str.startswith(invoice, 'BMBO-'))
+            assert str.startswith(invoice, 'BMBO-')
         api.deauth()
 
     def test_account_invoice_list_open(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         invoices = api.account_invoice_get_list_open(api_test_user)
-        self.assertIsNotNone(invoices)
+        assert invoices is not None
         # The test account does not have any open invoices
-        self.assertEqual(0, len(invoices))
+        assert len(invoices) == 0
 
     def test_domain_list(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         domains = api.domain_list(api_test_user)
         for domain in domains:
-            self.assertIsNotNone(domain['domain'], domain['count_mails'])
-            self.assertIsInstance(domain['domain'], str)
-            self.assertIsInstance(domain['count_mails'], int)
+            assert domain['domain'] is not None
+            assert domain['count_mails'] is not None
+            assert isinstance(domain['domain'], str)
+            assert isinstance(domain['count_mails'], int)
         api.deauth()
 
     def test_domain_get(self):
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         domains = api.domain_list(api_test_user)
-        self.assertEqual('testbmboapi.internal', domains[0]['domain'])
-        self.assertIsNotNone(domains[0]['count_mails'])
+        assert 'testbmboapi.internal' == domains[0]['domain']
+        assert domains[0]['count_mails'] is not None
         api.deauth()
 
     def test_mail_list(self):
@@ -137,20 +141,20 @@ class TestAPIClient(unittest.TestCase):
         api.auth(api_test_user, api_test_pass)
         domain = api.domain_list(api_test_user)[0]['domain']
         mails = api.mail_list(domain)
-        self.assertIsNotNone(mails)
-        self.assertIsNotNone(mails[0]['mail'])
-        self.assertIn('@' + domain, mails[0]['mail'])
-        self.assertEqual(mails[0]['parent_uid'], api_test_user)
-        self.assertIsNotNone(mails[0]['domain'])
-        self.assertIsNotNone(mails[0]['type'])
-        self.assertIsNotNone(mails[0]['memo'])
-        self.assertIsNotNone(mails[0]['forwards'])
-        self.assertIsNotNone(mails[0]['aliases'])
-        self.assertIsNotNone(mails[0]['capabilities'])
-        self.assertEqual(['MAIL_BLACKLIST', 'MAIL_SPAMPROTECTION', 'MAIL_PASSWORDRESET_SMS', 'MAIL_BACKUPRECOVER'],
-                         mails[0]['possible_capabilities'])
-        self.assertIn(mails[0]['plan'], ['premium', 'standard', 'light'])
-        self.assertIsNotNone(mails[0]['creation_date'])
+        assert mails is not None
+        assert mails[0]['mail'] is not None
+        assert '@' in mails[0]['mail'] and domain in mails[0]['mail']
+        assert mails[0]['parent_uid'] == api_test_user
+        assert mails[0]['domain'] is not None
+        assert mails[0]['type'] is not None
+        assert mails[0]['memo'] is not None
+        assert mails[0]['forwards'] is not None
+        assert mails[0]['aliases'] is not None
+        assert mails[0]['capabilities'] is not None
+        assert (mails[0]['possible_capabilities'] ==
+                ['MAIL_BLACKLIST', 'MAIL_SPAMPROTECTION', 'MAIL_PASSWORDRESET_SMS', 'MAIL_BACKUPRECOVER'])
+        assert mails[0]['plan'] in ['premium', 'standard', 'light']
+        assert mails[0]['creation_date'] is not None
         api.deauth()
 
     def test_mail_get(self):
@@ -159,18 +163,18 @@ class TestAPIClient(unittest.TestCase):
         domain = api.domain_list(api_test_user)[0]['domain']
         mails = api.mail_list(domain)
         mail = mails[0]
-        self.assertIn('@' + domain, mail['mail'])
-        self.assertEqual(api_test_user, mail['parent_uid'])
-        self.assertIn(domain, mail['domain'])
-        self.assertIn('inbox', mail['type'])
-        self.assertIsNotNone(mail['memo'])
-        self.assertIsNotNone(mail['forwards'])
-        self.assertIsNotNone(mail['aliases'])
-        self.assertIsNotNone(mail['capabilities'])
-        self.assertEqual(['MAIL_BLACKLIST', 'MAIL_SPAMPROTECTION', 'MAIL_PASSWORDRESET_SMS', 'MAIL_BACKUPRECOVER'],
-                         mail['possible_capabilities'])
-        self.assertIn(mail['plan'], ['premium', 'standard', 'light'])
-        self.assertIsNotNone(mail['creation_date'])
+        assert '@' in mail['mail'] and domain in mail['mail']
+        assert mail['parent_uid'] == api_test_user
+        assert mail['domain'] == domain
+        assert mail['type'] in ['inbox', 'inboxforward', 'forward']
+        assert mail['memo'] is not None
+        assert mail['forwards'] is not None
+        assert mail['aliases'] is not None
+        assert mail['capabilities'] is not None
+        assert (mail['possible_capabilities'] ==
+                ['MAIL_BLACKLIST', 'MAIL_SPAMPROTECTION', 'MAIL_PASSWORDRESET_SMS', 'MAIL_BACKUPRECOVER'])
+        assert mail['plan'] in ['premium', 'standard', 'light']
+        assert mail['creation_date'] is not None
         api.deauth()
 
     def test_mail_add(self):
@@ -181,7 +185,7 @@ class TestAPIClient(unittest.TestCase):
         mail_address = test_id + '@' + domain
 
         api.mail_add(mail_address, generate_pw(), 'standard', test_id, test_id)
-        self.assertEqual(api.mail_get(mail_address)['mail'], mail_address)
+        assert api.mail_get(mail_address)['mail'] == mail_address
 
     def test_mail_externaluid(self):
         api = APIClient.APIClient()
@@ -194,7 +198,7 @@ class TestAPIClient(unittest.TestCase):
         api.mail_set(mail, uid_extern=test_id)
         returned_mail = api.mail_externaluid(api_test_user, test_id)
 
-        self.assertEqual(returned_mail['mail'], mail)
+        assert returned_mail['mail'] == mail
         api.deauth()
 
     def test_mail_set(self):
@@ -222,7 +226,7 @@ class TestAPIClient(unittest.TestCase):
 
         # Compare values sent to values received
         for k, v in mail_set_tests.items():
-            self.assertEqual(check_mail[k], v)
+            assert check_mail[k] == v
 
         api.deauth()
 
@@ -234,9 +238,9 @@ class TestAPIClient(unittest.TestCase):
         mail = mails[0]['mail']
 
         api.mail_set_state(mail, False)
-        self.assertEqual(api.mail_get(mail)['active'], False)
+        assert api.mail_get(mail)['active'] == False
         api.mail_set_state(mail, True)
-        self.assertEqual(api.mail_get(mail)['active'], True)
+        assert api.mail_get(mail)['active'] == True
         api.deauth()
 
     def test_mail_set_plan(self):
@@ -248,6 +252,6 @@ class TestAPIClient(unittest.TestCase):
         plans = ['light', 'standard', 'premium']
         for plan in plans:
             api.mail_set_plan(mail, plan)
-            self.assertEqual(str(api.mail_get(mail)['plan']).lower(), plan)
+            assert str(api.mail_get(mail)['plan']).lower() == plan
         api.deauth()
 
