2d305e929f547007388c4da4e9f0aec3252d8295
heshsum <mail@heshsum.de>
2026-01-31 22:22:25 +0100

    Remove redundant test

--- tests/TestAPIClient.py
+++ tests/TestAPIClient.py
@@ -139,7 +139,6 @@ class TestAPIClient:
         api = APIClient.APIClient()
         api.auth(api_test_user, api_test_pass)
         domains = api.domain_list(api_test_user)
-        assert domains[0]['domain'] == domain
         assert domains[0]['count_mails'] is not None
         api.deauth()
 
