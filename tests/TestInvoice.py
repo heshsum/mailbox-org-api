36c86a5cc842b1ff7d68daf94f5517a8acda6ff0
heshsum <mail@heshsum.de>
2026-01-31 19:35:03 +0100

    Refactored TestInvoice to use pytest

--- tests/TestInvoice.py
+++ tests/TestInvoice.py
@@ -1,47 +1,46 @@
 from mailbox_org_api import Invoice
-import unittest
 
 test_account = 'test_account'
 test_id = 'BMBO-1234-2025'
 
-class TestMail(unittest.TestCase):
+class TestMail:
 
     def test_invoice_create(self):
         invoice = Invoice.Invoice(test_account, test_id)
-        self.assertEqual(invoice.account, test_account)
-        self.assertEqual(invoice.invoice_id, test_id)
+        assert invoice.account == test_account
+        assert invoice.invoice_id == test_id
 
     def test_invoice_account(self):
         invoice = Invoice.Invoice(test_account, test_id)
-        self.assertEqual(invoice.account, test_account)
+        assert invoice.account == test_account
         account = 'test_account2'
         invoice.account = account
-        self.assertEqual(invoice.account, account)
+        assert invoice.account == account
 
     def test_invoice_id(self):
         invoice = Invoice.Invoice(test_account, test_id)
-        self.assertEqual(invoice.invoice_id, test_id)
+        assert invoice.invoice_id == test_id
         id = 'BMBO-9876-25'
         invoice.invoice_id = id
-        self.assertEqual(invoice.invoice_id, id)
+        assert invoice.invoice_id, id
 
     def test_invoice_status(self):
         invoice = Invoice.Invoice(test_account, test_id)
-        self.assertEqual(invoice.status, None)
+        assert invoice.status is None
         status = 'open'
         invoice.status = status
-        self.assertEqual(invoice.status, status)
+        assert invoice.status == status
 
     def test_invoice_date(self):
         invoice = Invoice.Invoice(test_account, test_id)
-        self.assertEqual(invoice.date, None)
+        assert invoice.date is None
         date = '2025-12-31'
         invoice.date = date
-        self.assertEqual(invoice.date, date)
+        assert invoice.date == date
 
     def test_invoice_token(self):
         invoice = Invoice.Invoice(test_account, test_id)
-        self.assertEqual(invoice.token, None)
+        assert invoice.token is None
         token = '123456789'
         invoice.token = token
-        self.assertEqual(invoice.token, token)
+        assert invoice.token == token
