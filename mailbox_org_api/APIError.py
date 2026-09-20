f0c6f959d62c4db97596da611f2763d23dc3ae85
heshsum <mail@heshsum.de>
2026-09-20 18:13:31 +0200

    Added error message to APIError

--- mailbox_org_api/APIError.py
+++ mailbox_org_api/APIError.py
@@ -1,5 +1,7 @@
 class APIError(Exception):
     """Custom exception for API errors."""
 
-    def __init__(self, message, code=None):
+    def __init__(self, message: str, code: int | None = None):
+        self.message = message
+        self.code = code
         super().__init__(f'Error {code} - {message}')
